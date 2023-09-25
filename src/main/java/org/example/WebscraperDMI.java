d146278e38706e24b8c761807e79e7d6fde67391
martinthuren <cph-mc489@cphbusiness.dk>
2023-09-25 14:46:03 +0200

    .

--- src/main/java/org/example/WebscraperDMI.java
+++ src/main/java/org/example/WebscraperDMI.java
@@ -3,21 +3,33 @@ package org.example;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 public class WebscraperDMI {
+
     public static void main(String[] args) throws IOException {
-        scrape();
+            scrapeDMI();
     }
 
-    public static void scrape() throws IOException {
-        String url = "https://www.dmi.dk/lokation/show/DK/2618468/Kongens_Lyngby/";
+    public static void scrapeDMI() throws IOException {
+        String url = "https://www.vejreti.com/europe/denmark?page=14";
         Document doc = Jsoup.connect(url).get();
 
-        
+        String className = "weather_day_box ";
+
+        int count = 0;
+
+        for (Element row : doc.getElementsByClass(className)) {
+
+            final String day = row.getElementsByClass("weather_day_date").text();
+
+            final String dateMonth = row.getElementsByClass("weather_date_month").text();
+
+            final String temp = row.getElementsByClass("fourteen_day_wrap").text();
+
+            System.out.println(day + " " + dateMonth + " " + temp);
+        }
     }
 }
-
-
-
