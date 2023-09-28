6efa5d1c0e81183a3382d64714801ce9b08cb121
Ghostfear1713 <Uniton@live.dk>
2023-09-28 14:10:23 +0200

    Made a method to retrieve one column

--- src/main/java/Webscrapper.java
+++ src/main/java/Webscrapper.java
@@ -13,33 +13,38 @@ public class Webscrapper {
         Document doc = null;
 
         try{
-            doc = Jsoup.connect(url2).get();
+            doc = Jsoup.connect(url).get();
 
 //            //Gets first row
 //            String focusPoint = "#line1";
 //            Element row = doc.select(focusPoint).first();
 //            System.out.println(row.text());
 
-//            String secondFocusPoint = ".weather_table.day_table";
+            String secondFocusPoint = ".weather_table.day_table";
 //
 //
-//            Element table = doc.select(secondFocusPoint).first();
-//            System.out.println(table.text());
-//            //Elements rows = table.select("tr");
+            Element table = doc.select(secondFocusPoint).first();
+            Elements rows = table.select("tr");
+            for (Element row : rows) {
+                Elements columns = row.select("td"); // Select all columns in each row
+                if (!columns.isEmpty()) {
+                    Element column = columns.get(2); // Get the first column (change the index to select a different column)
+                    System.out.println(column.text()); // Print the text content of the column
+                }
+            }
 //
-//            String temperature = table.select("asdasd").forEach();
-//            String forecast = "";
+//
 
 
-            //Det område, vi ønsker data fra
-            String cssSelect = ".ipc-metadata-list.ipc-metadata-list--dividers-between.sc-3f13560f-0.sTTRj.compact-list-view.ipc-metadata-list--base";
-            //Det er den liste vi ønsker at få frem / værdier vi ønsker at få frem
-            Element table = doc.select(cssSelect).first();
-
-            //Søger vi specifikt efter årtal
-            table.select(".sc-e3e7b191-0.iKUUVe.sc-4dcdad14-2.bYaHFC.cli-ratings-container").forEach(info -> {
-                System.out.println(info.select(".ipc-rating-star.ipc-rating-star--base.ipc-rating-star--imdb.ratingGroup--imdb-rating").stream().skip(0).toList().get(0).text());
-            });
+//            //Det område, vi ønsker data fra
+//            String cssSelect = ".weather_table.day_table";
+//            //Det er den liste vi ønsker at få frem / værdier vi ønsker at få frem
+//            Element table = doc.select(cssSelect).first();
+//
+//            //Søger vi specifikt efter årtal
+//            table.select(".sc-e3e7b191-0.iKUUVe.sc-4dcdad14-2.bYaHFC.cli-ratings-container").forEach(info -> {
+//                System.out.println(info.select(".ipc-rating-star.ipc-rating-star--base.ipc-rating-star--imdb.ratingGroup--imdb-rating").stream().skip(0).toList().get(0).text());
+//            });
 
 
 
